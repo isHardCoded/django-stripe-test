0f9bdb994600cbaa16c3e1e4d6086fc74a99aeaf
isHardCoded <ishardcoded@gmail.com>
2025-12-30 19:09:41 +0300

    Реализовали API для buy и item

--- store/stripe_client.py
+++ store/stripe_client.py
@@ -1,4 +1,6 @@
 import stripe
 from django.conf import settings
 
-stripe.api_key = settings.STRIPE_SECRET_KEY
+def init_stripe():
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    return stripe
--- store/views.py
+++ store/views.py
@@ -1,3 +1,42 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.conf import settings
+from .models import Item
+from .stripe_client import init_stripe
 
-# Create your views here.
+stripe = init_stripe()
+
+def buy_item(request, id):
+    item = get_object_or_404(Item, id=id)
+
+    session = stripe.checkout.Session.create(
+        payment_method_types=["card"],
+        line_items=[{
+            "price_data": {
+                "currency": "usd",
+                "product_data": {
+                    "name": item.name,
+                    "description": item.description,
+                },
+                "unit_amount": item.price,
+            },
+            "quantity": 1,
+        }],
+        mode="payment",
+        success_url="http://localhost:8001/success/",
+        cancel_url="http://localhost:8001/cancel/",
+    )
+
+    return JsonResponse({"session_id": session.id})
+
+def item_detail(request, id):
+    item = get_object_or_404(Item, id=id)
+    return render(
+        request,
+        "store/item.html",
+        {
+            "item": item,
+            "stripe_public_key": settings.STRIPE_PUBLIC_KEY,
+        }
+    )
