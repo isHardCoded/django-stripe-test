10f3f80e8b6624bf98d041e676c987a7a3ea3749
isHardCoded <ishardcoded@gmail.com>
2025-12-30 19:31:46 +0300

    Подключили Stripe Payment для Order, пофиксили buy для item

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import buy_item, item_detail
+from .views import buy_item, item_detail, buy_order
 
 urlpatterns = [
-    path("buy/<int:id>/", buy_item, name="buy-item"),
+    path("buy_item/<int:id>/", buy_item, name="buy-item"),
+    path("buy_order/<int:id>/", buy_order, name="buy-order"),
     path("item/<int:id>/", item_detail, name="item-detail"),
 ]
--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.conf import settings
-from .models import Item
+from .models import Item, Order
 from .stripe_client import init_stripe
 
 stripe = init_stripe()
@@ -40,3 +40,30 @@ def item_detail(request, id):
             "stripe_public_key": settings.STRIPE_PUBLIC_KEY,
         }
     )
+
+def buy_order(request, id):
+    order = get_object_or_404(Order, id=id)
+    
+    line_items = [
+        {
+            "price_data": {
+                "currency": "usd",
+                "product_data": {
+                    "name": item.name,
+                    "description": item.description,
+                },
+                "unit_amount": item.price,
+            },
+            "quantity": 1,
+        } for item in order.items.all()
+    ]
+
+    session = stripe.checkout.Session.create(
+        payment_method_types=["card"],
+        line_items=line_items,
+        mode="payment",
+        success_url="http://localhost:8000/success/",
+        cancel_url="http://localhost:8000/cancel/",
+    )
+    
+    return JsonResponse({"session_id": session.id})
